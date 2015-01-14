e701af68be732d0ba0837cb3fcf575477b642642
Malachi Gray <graymd.cfi@gmail.com>
2015-01-14 17:45:43 -0500

    website built.  need to add some more word content.  need to create/update readme file

--- day8_server.rb
+++ day8_server.rb
@@ -1,8 +1,20 @@
 require 'sinatra'
 require 'faker'
 
+
+
 get '/' do
 	@fake_name = Faker::Name.name
+	@fake_about_me = Faker::Lorem.paragraph(4)
 	erb :index, layout: :site
+end
 
+get '/Stacey' do
+	@fake_name = Faker::Name.name
+	erb :stacey, layout: :site
+end
+
+get '/Randofriend' do
+	@fake_name = Faker::Name.name
+	erb :randofriend, layout: :site
 end
